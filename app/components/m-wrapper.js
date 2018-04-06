1435faca2c7722dd38ce1e5e07c57940ae35e4f4
Lex3960 <A01273960@itesm.mx>
2018-04-06 01:58:02 -0500

    Mejora de Estilos: JQuery

---
+++ app/components/m-wrapper.js
@@ -0,0 +1,17 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    tagName: 'span',
+    query: 'a',
+    method: 'sideNav',
+    options: null,
+    didInsertElement () {
+      var query = this.get('query');
+      var method = this.get('method');
+      var options = this.get('options');
+      if(typeof(options)=="string")
+        options = JSON.parse(options);
+      this.$(query)[method](options);
+      window.Materialize.updateTextFields();
+    }
+  });
